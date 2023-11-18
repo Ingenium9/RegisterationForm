71b3be05dc706e8bd755d7f554793a0a62cad26e
Shrishail Rugge <shrishailrugge@gmail.com>
2023-11-18 22:28:04 +0530

    Added the functionality of show and hide button in password and confirm password

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//input fields event listener 
 document.addEventListener('DOMContentLoaded', function () {
     const inputGroups = document.querySelectorAll('.input-group input');
 
@@ -11,11 +12,56 @@ document.addEventListener('DOMContentLoaded', function () {
     function handleInput(input) {
         const label = input.previousElementSibling;
         if (input.value.trim() !== '') {
-            label.classList.add('moved');
-            input.classList.add('color-change');
+            label.classList.add('moved'); //for placing the text in input field to the top
+            input.classList.add('color-change'); //changes the border color to blue
         } else {
             label.classList.remove('moved');
             input.classList.remove('color-change');
         }
     }
 });
+
+
+//Show button (password and confirm password field) and changing state of the text event listener
+document.addEventListener('DOMContentLoaded',function(){
+    const passwordInput=document.getElementById('pass');
+    const confirmPasswordInput=document.getElementById('cpass');
+    const passShowButton=document.getElementById('pass-show-button');
+    const cpassShowButton=document.getElementById('cpass-show-button');
+
+    passwordInput.addEventListener('input', function(){
+        checkShow(passwordInput,passShowButton);
+    });
+    confirmPasswordInput.addEventListener('input', function(){
+        checkShow(confirmPasswordInput,cpassShowButton);
+    });
+
+    function checkShow(input, showButton){
+        if(input.value.trim()!==''){
+            showButton.classList.add('visible');
+        }
+        else{
+            showButton.classList.remove('visible');
+        }
+    }
+
+    //enabling show and hide button with their functionalities
+    passShowButton.addEventListener('click',function(){
+        viewPassword(passwordInput,passShowButton);
+    });
+    cpassShowButton.addEventListener('click',function(){
+        viewPassword(confirmPasswordInput,cpassShowButton);
+    });
+
+    function viewPassword(input, showButton){
+        if(input.type==='password'){
+            input.type='text';
+            showButton.textContent='HIDE';
+        }
+        else{
+            input.type='password';
+            showButton.textContent='SHOW';
+        }
+    }
+});
+
